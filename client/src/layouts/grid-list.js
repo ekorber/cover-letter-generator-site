b63bcd3ad8565d1f0c2d5f54320111e9727daf03
Eric Korber <erickorber1994@gmail.com>
2024-01-31 02:02:09 -0500

    Create GridList layout component

--- client/src/layouts/grid-list.js
+++ client/src/layouts/grid-list.js
@@ -1,9 +1,19 @@
+import styled from 'styled-components'
 
+const List = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(${(props) => props.itemMinWidth}px, 1fr));
+    gap: ${(props) => props.gap}px;
+    width: 100%;
+    padding: ${(props) => props.padding}px;
+    justify-items: center;
+`;
 
-function GridList() {
+function GridList({ itemMinWidth=200, gap=10, padding=10, children }) {
     return (
-      <>
-      </>
+      <List itemMinWidth={itemMinWidth} gap={gap} padding={padding}>
+        {children}
+      </List>
     );
   }
   
