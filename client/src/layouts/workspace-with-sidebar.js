e6728dde74621e209da8860aeb2360a4d72d77a1
Eric Korber <erickorber1994@gmail.com>
2024-01-31 03:00:30 -0500

    Complete split layout UI component

--- client/src/layouts/workspace-with-sidebar.js
+++ client/src/layouts/workspace-with-sidebar.js
@@ -1,10 +1,45 @@
+import styled from 'styled-components'
 
+const LeftSplitLayout = styled.div`
+    display: grid;
+    grid-template-areas: 'sidebar content content content content';
+`;
 
-function WorkspaceWithSidebar() {
+const RightSplitLayout = styled.div`
+    display: grid;
+    grid-template-areas: 'content content content content sidebar';
+`;
+
+const Content = styled.div`
+    grid-area: content;
+`;
+
+const Sidebar = styled.aside`
+    grid-area: sidebar;
+`;
+
+export function LeftSidebarSplitLayout({children}) {
     return (
-      <>
-      </>
+      <LeftSplitLayout>
+        <Sidebar>
+          {children[0]}
+        </Sidebar>
+        <Content>
+          {children[1]}
+        </Content>
+      </LeftSplitLayout>
     );
   }
-  
-  export default WorkspaceWithSidebar;
+
+export function RightSidebarSplitLayout({children}) {
+    return (
+      <RightSplitLayout>
+        <Content>
+          {children[0]}
+        </Content>
+        <Sidebar>
+          {children[1]}
+        </Sidebar>
+      </RightSplitLayout>
+    );
+  }
