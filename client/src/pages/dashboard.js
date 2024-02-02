fad6d2c2dfc2b59680e7c33329ea78591147b618
Eric Korber <erickorber1994@gmail.com>
2024-02-02 18:00:07 -0500

    Populate the dashboard

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,9 +1,25 @@
-
+import SplitLayout from '../layouts/split-layout';
+import StickyScrollLayout from '../layouts/sticky-scroll-content';
+import PrimaryButton from '../buttons/btn-primary';
+import HistoryList from '../components/history-list';
+import TemplateGrid from '../components/template-grid';
 
 function DashboardPage() {
   return (
-    <>
-    </>
+    <SplitLayout split={20} className='p-5 gap-5'>
+      <div>
+        <PrimaryButton className='bg-blue-500 mb-4'>Manage Templates</PrimaryButton>
+        <StickyScrollLayout height='85dvh' className='rounded-lg bg-cyan-500 shadow-sm'>
+          <HistoryList />
+        </StickyScrollLayout>
+      </div>
+      <div>
+        <PrimaryButton className='bg-blue-500 mb-4 w-28 float-end'>Profile</PrimaryButton>
+        <StickyScrollLayout height='85dvh' className='mt-20 rounded-lg bg-cyan-500 shadow-lg'>
+          <TemplateGrid />
+        </StickyScrollLayout>
+      </div>
+    </SplitLayout>
   );
 }
 
