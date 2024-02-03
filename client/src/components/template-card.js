a95781d6973ef6e06b146a7fa54ab01926b6c6d5
Eric Korber <erickorber1994@gmail.com>
2024-02-02 20:27:46 -0500

    Add generate button to template card

--- client/src/components/template-card.js
+++ client/src/components/template-card.js
@@ -1,7 +1,12 @@
+import PrimaryButton from "../buttons/btn-primary";
+
 function TemplateCard({ name, showButtons=true }) {
     return (
-        <div className="bg-slate-100 w-56 h-56 flex items-center justify-items-center shadow-lg rounded-lg">
-            <p className='text-center text-slate-900 mx-auto max-w-48 text-shadow-lg'>{name}</p>
+        <div className="bg-slate-100 w-56 h-56 flex flex-col items-center shadow-lg rounded-lg">
+            <div className='my-auto'>
+                <p className='text-center text-slate-900 max-w-48 text-shadow-lg'>{name}</p>
+            </div>
+            <PrimaryButton className='w-28 mb-5 block'>Generate</PrimaryButton>
         </div>
     );
 }
