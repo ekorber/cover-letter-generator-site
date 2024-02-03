2eb1b73dc6683e7cbb8d90d67a4dccfb395c780e
Eric Korber <erickorber1994@gmail.com>
2024-02-02 19:34:03 -0500

    Add routes for all current pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import DashboardPage from './pages/dashboard';
+import HomePage from './pages/home';
+import LoginPage from './pages/login';
+import SignUpPage from './pages/signup';
+import ProfileSettingsPage from './pages/profile-settings';
+import TemplateEditorPage from './pages/template-editor';
 
 function App() {
   return (
-    <>
-      <DashboardPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/template-editor" element={<TemplateEditorPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/profile" element={<ProfileSettingsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
