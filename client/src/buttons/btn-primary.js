a5f1cb027d071a89ae24fe976ec231cd022d0cd6
Eric Korber <erickorber1994@gmail.com>
2024-02-02 20:01:27 -0500

    Set styling for primary button

--- client/src/buttons/btn-primary.js
+++ client/src/buttons/btn-primary.js
@@ -1,6 +1,6 @@
 function PrimaryButton({ onClick, className, children }) {
   return (
-    <button onClick={() => {if (onClick) {onClick()}}} className={`h-16 rounded-lg ${className}`}>{children}</button>
+    <button onClick={() => {if (onClick) {onClick()}}} className={`h-16 rounded-lg bg-gradient-to-b from-blue-400 to-blue-500 text-slate-100 font-medium shadow-lg ${className}`}>{children}</button>
   );
 }
 
