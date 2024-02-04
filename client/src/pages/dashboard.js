9b471e6042cf555aac590f3a8401ea6995ea2ae8
Eric Korber <erickorber1994@gmail.com>
2024-02-04 14:51:16 -0500

    Rearrange layout, link top menu buttons to pages, adjust styling

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,23 +1,24 @@
+import { Link } from 'react-router-dom';
 import SplitLayout from '../layouts/split-layout';
 import StickyScrollLayout from '../layouts/sticky-scroll-content';
 import PrimaryButton from '../buttons/btn-primary';
 import HistoryList from '../components/history-list';
 import TemplateGrid from '../components/template-grid';
-import { Link } from 'react-router-dom';
+import { green500, green700 } from '../colors';
 
 function DashboardPage() {
   return (
-    <SplitLayout split={20} className='p-5 gap-5'>
+    <SplitLayout split={80} className='p-5 gap-5'>
       <div>
-        <Link to="/template-editor"><PrimaryButton className='mb-4 w-full'>Manage Templates</PrimaryButton></Link>
-        <StickyScrollLayout height='85dvh' className='rounded-lg bg-cyan-500 shadow-sm'>
-          <HistoryList />
+        <Link to="/template-editor"><PrimaryButton className='mb-4 w-56'>Manage Templates</PrimaryButton></Link>
+        <StickyScrollLayout height='85dvh' className='rounded-lg bg-cyan-400 shadow-lg'>
+          <TemplateGrid />
         </StickyScrollLayout>
       </div>
       <div>
-        <Link to="/profile"><PrimaryButton className='mb-4 w-28 float-end'>Profile</PrimaryButton></Link>
-        <StickyScrollLayout height='85dvh' className='mt-20 rounded-lg bg-cyan-500 shadow-lg'>
-          <TemplateGrid />
+        <Link to="/profile"><PrimaryButton fromColor={green500} toColor={green700} className='mb-4 w-full'>Profile</PrimaryButton></Link>
+        <StickyScrollLayout height='85dvh' className='rounded-lg bg-emerald-300 shadow-sm'>
+          <HistoryList />
         </StickyScrollLayout>
       </div>
     </SplitLayout>
