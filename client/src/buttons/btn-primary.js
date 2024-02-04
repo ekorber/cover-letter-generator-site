17008fe298d0e272257699dbb76b28c56772e7d5
Eric Korber <erickorber1994@gmail.com>
2024-02-04 16:13:41 -0500

    Add type parameter to primary button

--- client/src/buttons/btn-primary.js
+++ client/src/buttons/btn-primary.js
@@ -4,9 +4,10 @@ const Button = styled.button`
   background: linear-gradient(to bottom, ${({fromColor}) => fromColor}, ${({toColor}) => toColor});
 `;
 
-function PrimaryButton({ onClick, fromColor='rgb(59 130 246)', toColor='rgb(29 78 216)', className, children }) {
+function PrimaryButton({ onClick, type='button', fromColor='rgb(59 130 246)', toColor='rgb(29 78 216)', className, children }) {
   return (
     <Button onClick={() => {if (onClick) {onClick()}}}
+            type={type}
             fromColor={fromColor} 
             toColor={toColor} 
             className={`h-16 rounded-lg text-slate-100 font-medium shadow-lg ${className}`}>{children}</Button>
