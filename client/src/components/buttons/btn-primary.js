a50bc2091f87e23e413f9672f155bbdf9670a0c0
Eric Korber <erickorber1994@gmail.com>
2024-02-08 16:24:03 -0500

    Fix console errors

--- client/src/components/buttons/btn-primary.js
+++ client/src/components/buttons/btn-primary.js
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 
 const Button = styled.button`
-  background: linear-gradient(to bottom, ${({fromColor}) => fromColor}, ${({toColor}) => toColor});
+  background: linear-gradient(to bottom, ${({fromcolor}) => fromcolor}, ${({tocolor}) => tocolor});
 
   &:hover {
-    background: linear-gradient(to bottom, ${({hoverFromColor}) => hoverFromColor}, ${({hoverToColor}) => hoverToColor});
+    background: linear-gradient(to bottom, ${({hoverfromcolor}) => hoverfromcolor}, ${({hovertocolor}) => hovertocolor});
   }
 `;
 
@@ -15,18 +15,18 @@ function PrimaryButton({onClick, to='', type='button', fromColor, toColor, hover
       {to ? 
         <Button onClick={() => {if (onClick) {onClick()}}}
           type={type}
-          fromColor={fromColor} 
-          toColor={toColor} 
-          hoverFromColor={hoverFromColor}
-          hoverToColor={hoverToColor}
+          fromcolor={fromColor} 
+          tocolor={toColor} 
+          hoverfromcolor={hoverFromColor}
+          hovertocolor={hoverToColor}
           className={`h-16 rounded-lg text-slate-100 font-medium shadow-lg ${className}`}><Link to={to}>{children}</Link></Button>
         :
         <Button onClick={() => {if (onClick) {onClick()}}}
               type={type}
-              fromColor={fromColor} 
-              toColor={toColor} 
-              hoverFromColor={hoverFromColor}
-              hoverToColor={hoverToColor}
+              fromcolor={fromColor} 
+              tocolor={toColor} 
+              hoverfromcolor={hoverFromColor}
+              hovertocolor={hoverToColor}
               className={`h-16 rounded-lg text-slate-100 font-medium shadow-lg ${className}`}>{children}</Button>
       }
     </>
--- client/src/layouts/grid-list.js
+++ client/src/layouts/grid-list.js
@@ -2,14 +2,14 @@ import styled from 'styled-components'
 
 const List = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(${(props) => props.itemMinWidth}px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(${(props) => props.itemminwidth}px, 1fr));
     gap: ${(props) => props.gap}px;
     justify-items: center;
 `;
 
 function GridListLayout({ itemMinWidth=200, gap=10, className, children }) {
   return (
-    <List itemMinWidth={itemMinWidth} gap={gap} className={`${className}`}>
+    <List itemminwidth={itemMinWidth} gap={gap} className={`${className}`}>
       {children}
     </List>
   );
