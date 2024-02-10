97561f7639d95da9643b335594d1628339224075
Eric Korber <erickorber1994@gmail.com>
2024-02-10 17:25:43 -0500

    Implement cover letter document generation

--- client/src/components/generate-cover-letter-form.js
+++ client/src/components/generate-cover-letter-form.js
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react"
+import { Document, Packer, Paragraph, TextRun } from 'docx';
+import { v4 as uuidv4 } from 'uuid';
+import { saveAs } from 'file-saver';
 import BlueButton from "./buttons/btn-blue"
 import RedButton from "./buttons/btn-red"
 
 function GenerateCoverLetterForm({ template, closeModal }) {
 
-    const [inputValues, setInputValues] = useState([])
+    const [inputValues, setInputValues] = useState([
+        {
+            id: uuidv4(),
+            varName: 'Position',
+            varValue: '',
+        },
+        {
+            id: uuidv4(),
+            varName: 'Company',
+            varValue: '',
+        },
+    ])
 
     useEffect(() => {
-        setInputValues(template.variables)
+        const newValues = [...inputValues, ...template.variables]
+        setInputValues(newValues)
     }, [])
 
     function handleChange(e, id) {
@@ -22,8 +37,85 @@ function GenerateCoverLetterForm({ template, closeModal }) {
 
     function handleSubmission(e) {
         e.preventDefault()
-        console.log('Submitted')
-        console.log(inputValues)
+
+        let coverLetterBody = template.body
+        let position = ''
+        let company = ''
+
+        // Get formatted date
+        const now = new Date()
+        const year = now.getFullYear()
+        const month = now.getMonth()
+        const day = now.getDate()
+        let formattedMonth = ''
+
+        switch (month) {
+            case 0: formattedMonth = 'January'; break;
+            case 1: formattedMonth = 'February'; break;
+            case 2: formattedMonth = 'March'; break;
+            case 3: formattedMonth = 'April'; break;
+            case 4: formattedMonth = 'May'; break;
+            case 5: formattedMonth = 'June'; break;
+            case 6: formattedMonth = 'July'; break;
+            case 7: formattedMonth = 'August'; break;
+            case 8: formattedMonth = 'September'; break;
+            case 9: formattedMonth = 'October'; break;
+            case 10: formattedMonth = 'November'; break;
+            case 11: formattedMonth = 'December'; break;
+        }
+
+        //Replace standard placeholders
+        coverLetterBody = coverLetterBody.replace('[Current Date]', `${formattedMonth} ${day}, ${year}`)
+
+        //Replace variable placeholders with their respective values
+        inputValues.forEach(element => {
+
+            if (element.varName === 'Company') {
+                company = element.varValue
+            }
+
+            if (element.varName === 'Position') {
+                position = element.varValue
+            } 
+
+            const regexPattern = `\\[${element.varName}\\]`
+            const regex = new RegExp(regexPattern, "g");
+            coverLetterBody = coverLetterBody.replace(regex, element.varValue)
+        })
+
+        // Seperate template body into paragraph parts
+        const parts = coverLetterBody.split(/\r?\n/)
+
+        // Create Paragraph objects, containing the previous parts
+        const paragraphs = []
+        parts.forEach(element => {
+            paragraphs.push(
+                new Paragraph({
+                    children: [
+                        new TextRun({
+                            text: element.trim(),
+                            size: "12pt",
+                        }),
+                    ]
+                })
+            )
+        });
+
+        // Create the Word document object, and place the paragraph objects inside
+        const doc = new Document({
+            sections: [{
+            children: [
+                ...paragraphs,
+            ],
+            }],
+        });
+        
+        // Generate the actual Word document
+        Packer.toBlob(doc).then(blob => {
+            // Save the document using FileSaver
+            saveAs(blob, `Cover Letter - ${company} - ${position}.docx`);
+        });
+        
         closeModal()
     }
 
