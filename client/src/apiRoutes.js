43d2946570cdcc503e2815d4cf766de9c77bf351
Eric Korber <erickorber1994@gmail.com>
2024-02-10 20:54:18 -0500

    Add new api route to front end

--- client/src/apiRoutes.js
+++ client/src/apiRoutes.js
@@ -2,6 +2,7 @@ export const API_USER_PROFILE = '/api/user/profile'
 export const API_USER_PROFILE_UPDATE = '/api/user/profile/update'
 
 export const API_USER_HISTORY_COVER_LETTERS = '/api/user/history/cover-letters'
+export const API_USER_HISTORY_COVER_LETTERS_SUBMIT = '/api/user/history/cover-letters/submit'
 export const API_USER_HISTORY_COVER_LETTERS_DELETE = '/api/user/history/cover-letters/delete'
 
 export const API_USER_TEMPLATES = '/api/user/templates'
