bef00eec2450a71e73b94ed96e612206c9050a8a
Eric Korber <erickorber1994@gmail.com>
2024-02-12 00:19:54 -0500

    Add info message to empty cover letter list area

--- client/src/components/history-list.js
+++ client/src/components/history-list.js
@@ -21,6 +21,7 @@ function HistoryList() {
 
     return (
         <div className='p-5 h-full bg-gradient-to-b from-green-600 overscroll-contain'>
+            {coverLetterHistory.length === 0 && <p className='text-slate-100 text-center italic mt-3 text-lg'>No cover letters generated yet!</p>}
             {coverLetterHistory.map(item => (
                 <HistoryListItem key={item.id} coverLetterData={item} />
             ))}
