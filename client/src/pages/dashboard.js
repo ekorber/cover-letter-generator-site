476e2aef880645b84ef4b94bb5ab0d2dc46c2567
Eric Korber <erickorber1994@gmail.com>
2024-02-12 02:19:32 -0500

    Make dynamic full screen height calculation for layouts

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -8,15 +8,21 @@ import MobileMenu from '../components/menus/mobile-menu';
 
 function DashboardPage() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [windowHeight, setWindowHeight] = useState(window.innerWidth)
 
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight)
     };
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
+
+  function getAdjustedWindowHeight(adjustmentSize) {
+    return (window.innerHeight - adjustmentSize) + 'px'
+  }
 
   return (
     <>
@@ -26,27 +32,23 @@ function DashboardPage() {
           <div>
             <p className='text-2xl px-9 pt-6 pb-5 font-bold'>COVER LETTERS</p>
             <SplitLayout split={80} className='px-5 pb-5 gap-5'>
-              <div className=''>
-                <StickyScrollLayout height='87dvh' className='rounded-lg bg-cyan-400 shadow-lg'>
-                  <TemplateGrid />
-                </StickyScrollLayout>
-              </div>
-              <div className=''>
-                <StickyScrollLayout height='87dvh' className='rounded-lg bg-emerald-300 shadow-sm'>
-                  <HistoryList />
-                </StickyScrollLayout>
-              </div>
+              <StickyScrollLayout height={() => getAdjustedWindowHeight(100)} className='rounded-lg bg-cyan-400 shadow-lg'>
+                <TemplateGrid/>
+              </StickyScrollLayout>
+              <StickyScrollLayout height={() => getAdjustedWindowHeight(100)} className='rounded-lg bg-emerald-300 shadow-sm'>
+                <HistoryList />
+              </StickyScrollLayout>
             </SplitLayout>
           </div>
         :
           <Tabs className='p-5'>
             <Tab label='Templates'>
-              <StickyScrollLayout height='85dvh' className='rounded-lg bg-cyan-400 shadow-lg'>
+              <StickyScrollLayout height={() => getAdjustedWindowHeight(120)} className='rounded-lg bg-cyan-400 shadow-lg'>
                 <TemplateGrid />
               </StickyScrollLayout>
             </Tab>
             <Tab label='Documents'>
-              <StickyScrollLayout height='85dvh' className='rounded-lg bg-emerald-300 shadow-sm'>
+              <StickyScrollLayout height={() => getAdjustedWindowHeight(120)} className='rounded-lg bg-emerald-300 shadow-sm'>
                 <HistoryList />
               </StickyScrollLayout>
             </Tab>
